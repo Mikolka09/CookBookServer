16699b01c717473ec9e922bad096e787575b8512
Микола Матіос <mikolka09@gmail.com>
2021-03-17 00:45:04 +0200

    Добавьте файлы проекта.

---
+++ CookBookServer/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace CookBookServer
+{
+    class Program
+    {
+        private static IPAddress iP;
+        private static IPEndPoint IPEnd;
+        private static Socket server;
+        private static Socket client;
+        private static string adress = "127.0.0.1";
+        private static int port = 8586;
+        private static string products = "";
+        private static List<CookBook> cookBooks;
+
+        static void Main(string[] args)
+        {
+            iP = IPAddress.Parse(adress);
+            IPEnd = new IPEndPoint(iP, port);
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            cookBooks = new List<CookBook>();
+            server.Bind(IPEnd);
+            server.Listen(10);
+
+            Task.Run(() =>
+            {
+                try 
+                { 
+                while (true)
+                {
+                    client = server.Accept();
+                    Task.Run(() =>
+                    {
+                        byte[] buff = new byte[4];
+                        int bytesLenght = BitConverter.ToInt32(buff, 0);
+                        byte[] data = new byte[bytesLenght];
+                        client.Receive(data);
+                        products = Encoding.Unicode.GetString(data);
+                    });
+                }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            });
+            Console.ReadKey();
+            Console.WriteLine("Сервер закрывается....");
+            server.Close();
+        }
+
+        private static void SearchRecipe(string products)
+        {
+
+        }
+    }
+
+    public class CookBook
+    {
+        public int IdRecipe;
+        public string NameRecipe;
+        public List<string> ListProducts;
+        public string Recipe;
+    }
+}
---
+++ ListRecipe/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
+using System.IO;
+
+namespace ListRecipes
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ListCookBooks listRecipes = new ListCookBooks();
+            listRecipes.cookBooks = new List<CookBook>();
+            bool end = true;
+            int i = 100;
+            while (end)
+            {
+                Console.Clear();
+                CookBook cook = new CookBook();
+                Console.WriteLine("\tСоздание рецепта\n");
+                Console.WriteLine();
+                cook.IdRecipe = i;
+                Console.Write("Введите название рецепта: ");
+                cook.NameRecipe = Console.ReadLine();
+                Console.Write("Введите перечень продуктов через пробел: ");
+                string prods = Console.ReadLine();
+                string[] prod = prods.Split();
+                cook.ListProducts = new List<string>();
+                foreach (var item in prod)
+                {
+                    cook.ListProducts.Add(item);
+                }
+                Console.WriteLine("Введите рецепт приготовления: ");
+                cook.Recipe = Console.ReadLine();
+                listRecipes.cookBooks.Add(cook);
+                Console.WriteLine();
+                Console.Write("Добавить еще один рецепт (Да-1, Нет-2): ");
+                int var = Convert.ToInt32(Console.ReadLine());
+                if (var == 1)
+                {
+                    end = true;
+                    i++;
+                }
+                else
+                    end = false;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Список рецептов создан!");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("\tСохраняем книгу список рецептов в файл\n");
+            Console.Write("Введите имя файла: ");
+            string nameFile = Console.ReadLine() + ".dat";
+            BinaryFormatter binary = new BinaryFormatter();
+            using (FileStream fs = new FileStream(nameFile, FileMode.OpenOrCreate))
+            {
+                binary.Serialize(fs, listRecipes);
+            }
+            Console.WriteLine("\nСписок рецептов сохранен!");
+
+            Console.ReadKey();
+        }
+    }
+
+    [Serializable]
+    public class CookBook
+    {
+        public int IdRecipe;
+        public string NameRecipe;
+        public List<string> ListProducts;
+        public string Recipe;
+    }
+
+    [Serializable]
+    public class ListCookBooks
+    {
+        public List<CookBook> cookBooks;
+    }
+}
